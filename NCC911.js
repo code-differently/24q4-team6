2bddc74ff8570d594608cad7f1bd8f481030e279
cdbluejr <cdblue244@gmail.com>
2025-02-13 18:40:13 +0000

    Capstone: Start 13-02

--- NCC911.js
+++ NCC911.js
@@ -11,6 +11,21 @@ app.get('/', (req, res) => {
     res.send('Holy Crap, Batman!!');
 });
 
+app.listen(5501, () => {
+    app.post("/submit", (req, res) => {
+    const { "FullName": fullName, "locationIncident": locationIncident, "phone-number": phoneNumber,"Addition Information": Information } = req.body;
+    
+    console.log("Form submitted:", { fullName, locationIncident, phoneNumber, Information });
+  
+    res.send(`
+      <h1>Form Submitted Successfully!</h1>
+      <p><strong>Full Name:</strong> ${fullName}</p>
+      <p><strong>Location of Incident:</strong> ${locationIncident}</p>
+      <p><strong>Phone Number:</strong> ${phoneNumber}</p>
+      <p><strong>Additional Information:</strong> ${Information}</p>
+    `);
+  });
+});
 app.listen(5500, () => {
     console.log(`Server is running on port ${port}`);
 });
